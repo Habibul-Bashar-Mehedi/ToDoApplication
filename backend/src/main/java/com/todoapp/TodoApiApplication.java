2c10e7c2d0e6f9099f9ded07317ccc2e510d89dd
habibul-bashar-mehedi <habibulbashar.cse@gmail.com>
2026-05-01 14:47:49 +0600

    Initialize backend Spring Boot project

---
+++ backend/src/main/java/com/todoapp/TodoApiApplication.java
@@ -0,0 +1,14 @@
+package com.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiApplication.class, args);
+    }
+}
